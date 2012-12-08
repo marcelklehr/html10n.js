a6b062e4775ced647b4c1c44cefa738cc47fb0e8
Marcel Klehr <mklehr@gmx.net>
2012-12-08 13:39:40 +0100

    Add macros, trl8.get and its alias window._

--- l10n.js
+++ l10n.js
@@ -151,6 +151,445 @@ window.trl8 = (function(window, document, undefined) {
   }
   MicroEvent.mixin(trl8)
   
+  trl8.macros = {}
+  
+  /**
+   * Get rules for plural forms (shared with JetPack), see:
+   * http://unicode.org/repos/cldr-tmp/trunk/diff/supplemental/language_plural_rules.html
+   * https://github.com/mozilla/addon-sdk/blob/master/python-lib/plural-rules-generator.p
+   *
+   * @param {string} lang
+   *    locale (language) used.
+   *
+   * @return {Function}
+   *    returns a function that gives the plural form name for a given integer:
+   *       var fun = getPluralRules('en');
+   *       fun(1)    -> 'one'
+   *       fun(0)    -> 'other'
+   *       fun(1000) -> 'other'.
+   */
+  function getPluralRules(lang) {
+    var locales2rules = {
+      'af': 3,
+      'ak': 4,
+      'am': 4,
+      'ar': 1,
+      'asa': 3,
+      'az': 0,
+      'be': 11,
+      'bem': 3,
+      'bez': 3,
+      'bg': 3,
+      'bh': 4,
+      'bm': 0,
+      'bn': 3,
+      'bo': 0,
+      'br': 20,
+      'brx': 3,
+      'bs': 11,
+      'ca': 3,
+      'cgg': 3,
+      'chr': 3,
+      'cs': 12,
+      'cy': 17,
+      'da': 3,
+      'de': 3,
+      'dv': 3,
+      'dz': 0,
+      'ee': 3,
+      'el': 3,
+      'en': 3,
+      'eo': 3,
+      'es': 3,
+      'et': 3,
+      'eu': 3,
+      'fa': 0,
+      'ff': 5,
+      'fi': 3,
+      'fil': 4,
+      'fo': 3,
+      'fr': 5,
+      'fur': 3,
+      'fy': 3,
+      'ga': 8,
+      'gd': 24,
+      'gl': 3,
+      'gsw': 3,
+      'gu': 3,
+      'guw': 4,
+      'gv': 23,
+      'ha': 3,
+      'haw': 3,
+      'he': 2,
+      'hi': 4,
+      'hr': 11,
+      'hu': 0,
+      'id': 0,
+      'ig': 0,
+      'ii': 0,
+      'is': 3,
+      'it': 3,
+      'iu': 7,
+      'ja': 0,
+      'jmc': 3,
+      'jv': 0,
+      'ka': 0,
+      'kab': 5,
+      'kaj': 3,
+      'kcg': 3,
+      'kde': 0,
+      'kea': 0,
+      'kk': 3,
+      'kl': 3,
+      'km': 0,
+      'kn': 0,
+      'ko': 0,
+      'ksb': 3,
+      'ksh': 21,
+      'ku': 3,
+      'kw': 7,
+      'lag': 18,
+      'lb': 3,
+      'lg': 3,
+      'ln': 4,
+      'lo': 0,
+      'lt': 10,
+      'lv': 6,
+      'mas': 3,
+      'mg': 4,
+      'mk': 16,
+      'ml': 3,
+      'mn': 3,
+      'mo': 9,
+      'mr': 3,
+      'ms': 0,
+      'mt': 15,
+      'my': 0,
+      'nah': 3,
+      'naq': 7,
+      'nb': 3,
+      'nd': 3,
+      'ne': 3,
+      'nl': 3,
+      'nn': 3,
+      'no': 3,
+      'nr': 3,
+      'nso': 4,
+      'ny': 3,
+      'nyn': 3,
+      'om': 3,
+      'or': 3,
+      'pa': 3,
+      'pap': 3,
+      'pl': 13,
+      'ps': 3,
+      'pt': 3,
+      'rm': 3,
+      'ro': 9,
+      'rof': 3,
+      'ru': 11,
+      'rwk': 3,
+      'sah': 0,
+      'saq': 3,
+      'se': 7,
+      'seh': 3,
+      'ses': 0,
+      'sg': 0,
+      'sh': 11,
+      'shi': 19,
+      'sk': 12,
+      'sl': 14,
+      'sma': 7,
+      'smi': 7,
+      'smj': 7,
+      'smn': 7,
+      'sms': 7,
+      'sn': 3,
+      'so': 3,
+      'sq': 3,
+      'sr': 11,
+      'ss': 3,
+      'ssy': 3,
+      'st': 3,
+      'sv': 3,
+      'sw': 3,
+      'syr': 3,
+      'ta': 3,
+      'te': 3,
+      'teo': 3,
+      'th': 0,
+      'ti': 4,
+      'tig': 3,
+      'tk': 3,
+      'tl': 4,
+      'tn': 3,
+      'to': 0,
+      'tr': 0,
+      'ts': 3,
+      'tzm': 22,
+      'uk': 11,
+      'ur': 3,
+      've': 3,
+      'vi': 0,
+      'vun': 3,
+      'wa': 4,
+      'wae': 3,
+      'wo': 0,
+      'xh': 3,
+      'xog': 3,
+      'yo': 0,
+      'zh': 0,
+      'zu': 3
+    };
+
+    // utility functions for plural rules methods
+    function isIn(n, list) {
+      return list.indexOf(n) !== -1;
+    }
+    function isBetween(n, start, end) {
+      return start <= n && n <= end;
+    }
+
+    // list of all plural rules methods:
+    // map an integer to the plural form name to use
+    var pluralRules = {
+      '0': function(n) {
+        return 'other';
+      },
+      '1': function(n) {
+        if ((isBetween((n % 100), 3, 10)))
+          return 'few';
+        if (n === 0)
+          return 'zero';
+        if ((isBetween((n % 100), 11, 99)))
+          return 'many';
+        if (n == 2)
+          return 'two';
+        if (n == 1)
+          return 'one';
+        return 'other';
+      },
+      '2': function(n) {
+        if (n !== 0 && (n % 10) === 0)
+          return 'many';
+        if (n == 2)
+          return 'two';
+        if (n == 1)
+          return 'one';
+        return 'other';
+      },
+      '3': function(n) {
+        if (n == 1)
+          return 'one';
+        return 'other';
+      },
+      '4': function(n) {
+        if ((isBetween(n, 0, 1)))
+          return 'one';
+        return 'other';
+      },
+      '5': function(n) {
+        if ((isBetween(n, 0, 2)) && n != 2)
+          return 'one';
+        return 'other';
+      },
+      '6': function(n) {
+        if (n === 0)
+          return 'zero';
+        if ((n % 10) == 1 && (n % 100) != 11)
+          return 'one';
+        return 'other';
+      },
+      '7': function(n) {
+        if (n == 2)
+          return 'two';
+        if (n == 1)
+          return 'one';
+        return 'other';
+      },
+      '8': function(n) {
+        if ((isBetween(n, 3, 6)))
+          return 'few';
+        if ((isBetween(n, 7, 10)))
+          return 'many';
+        if (n == 2)
+          return 'two';
+        if (n == 1)
+          return 'one';
+        return 'other';
+      },
+      '9': function(n) {
+        if (n === 0 || n != 1 && (isBetween((n % 100), 1, 19)))
+          return 'few';
+        if (n == 1)
+          return 'one';
+        return 'other';
+      },
+      '10': function(n) {
+        if ((isBetween((n % 10), 2, 9)) && !(isBetween((n % 100), 11, 19)))
+          return 'few';
+        if ((n % 10) == 1 && !(isBetween((n % 100), 11, 19)))
+          return 'one';
+        return 'other';
+      },
+      '11': function(n) {
+        if ((isBetween((n % 10), 2, 4)) && !(isBetween((n % 100), 12, 14)))
+          return 'few';
+        if ((n % 10) === 0 ||
+            (isBetween((n % 10), 5, 9)) ||
+            (isBetween((n % 100), 11, 14)))
+          return 'many';
+        if ((n % 10) == 1 && (n % 100) != 11)
+          return 'one';
+        return 'other';
+      },
+      '12': function(n) {
+        if ((isBetween(n, 2, 4)))
+          return 'few';
+        if (n == 1)
+          return 'one';
+        return 'other';
+      },
+      '13': function(n) {
+        if ((isBetween((n % 10), 2, 4)) && !(isBetween((n % 100), 12, 14)))
+          return 'few';
+        if (n != 1 && (isBetween((n % 10), 0, 1)) ||
+            (isBetween((n % 10), 5, 9)) ||
+            (isBetween((n % 100), 12, 14)))
+          return 'many';
+        if (n == 1)
+          return 'one';
+        return 'other';
+      },
+      '14': function(n) {
+        if ((isBetween((n % 100), 3, 4)))
+          return 'few';
+        if ((n % 100) == 2)
+          return 'two';
+        if ((n % 100) == 1)
+          return 'one';
+        return 'other';
+      },
+      '15': function(n) {
+        if (n === 0 || (isBetween((n % 100), 2, 10)))
+          return 'few';
+        if ((isBetween((n % 100), 11, 19)))
+          return 'many';
+        if (n == 1)
+          return 'one';
+        return 'other';
+      },
+      '16': function(n) {
+        if ((n % 10) == 1 && n != 11)
+          return 'one';
+        return 'other';
+      },
+      '17': function(n) {
+        if (n == 3)
+          return 'few';
+        if (n === 0)
+          return 'zero';
+        if (n == 6)
+          return 'many';
+        if (n == 2)
+          return 'two';
+        if (n == 1)
+          return 'one';
+        return 'other';
+      },
+      '18': function(n) {
+        if (n === 0)
+          return 'zero';
+        if ((isBetween(n, 0, 2)) && n !== 0 && n != 2)
+          return 'one';
+        return 'other';
+      },
+      '19': function(n) {
+        if ((isBetween(n, 2, 10)))
+          return 'few';
+        if ((isBetween(n, 0, 1)))
+          return 'one';
+        return 'other';
+      },
+      '20': function(n) {
+        if ((isBetween((n % 10), 3, 4) || ((n % 10) == 9)) && !(
+            isBetween((n % 100), 10, 19) ||
+            isBetween((n % 100), 70, 79) ||
+            isBetween((n % 100), 90, 99)
+            ))
+          return 'few';
+        if ((n % 1000000) === 0 && n !== 0)
+          return 'many';
+        if ((n % 10) == 2 && !isIn((n % 100), [12, 72, 92]))
+          return 'two';
+        if ((n % 10) == 1 && !isIn((n % 100), [11, 71, 91]))
+          return 'one';
+        return 'other';
+      },
+      '21': function(n) {
+        if (n === 0)
+          return 'zero';
+        if (n == 1)
+          return 'one';
+        return 'other';
+      },
+      '22': function(n) {
+        if ((isBetween(n, 0, 1)) || (isBetween(n, 11, 99)))
+          return 'one';
+        return 'other';
+      },
+      '23': function(n) {
+        if ((isBetween((n % 10), 1, 2)) || (n % 20) === 0)
+          return 'one';
+        return 'other';
+      },
+      '24': function(n) {
+        if ((isBetween(n, 3, 10) || isBetween(n, 13, 19)))
+          return 'few';
+        if (isIn(n, [2, 12]))
+          return 'two';
+        if (isIn(n, [1, 11]))
+          return 'one';
+        return 'other';
+      }
+    };
+
+    // return a function that gives the plural form name for a given integer
+    var index = locales2rules[lang.replace(/-.*$/, '')];
+    if (!(index in pluralRules)) {
+      consoleWarn('plural form unknown for [' + lang + ']');
+      return function() { return 'other'; };
+    }
+    return pluralRules[index];
+  }
+
+  /**
+   * pre-defined 'plural' macro
+   */
+  trl8.macros.plural = function(translations, key, str, param) {
+    var n = parseFloat(param);
+    if (isNaN(n))
+      return str;
+
+    // initialize _pluralRules
+    if (!this._pluralRules)
+      this._pluralRules = getPluralRules(trl8.language);
+    var index = '[' + this._pluralRules(n) + ']';
+
+    // try to find a [zero|one|two] key if it's defined
+    if (n === 0 && (key + '[zero]') in translations) {
+      str = translations[key + '[zero]'];
+    } else if (n == 1 && (key + '[one]') in translations) {
+      str = translations[key + '[one]'];
+    } else if (n == 2 && (key + '[two]') in translations) {
+      str = translations[key + '[two]'];
+    } else if ((key + index) in translations) {
+      str = translations[key + index][prop];
+    }
+
+    return str;
+  };
+  
   /**
    * Localize a document
    * @param langs An array of lang codes defining fallbacks
@@ -160,6 +599,7 @@ window.trl8 = (function(window, document, undefined) {
     if ('string' == typeof langs) langs = [langs]
     
     this.build(langs, function(er, translations) {
+      trl8.translations = translations
       trl8.translateElement(translations)
     })
   }
@@ -207,6 +647,71 @@ window.trl8 = (function(window, document, undefined) {
     return element.querySelectorAll('*[data-l10n-id]')
   }
   
+  trl8.get = function(id, args) {
+    var translations = trl8.translations
+    if(!translations[id]) return consoleWarn('Could not find string '+id)
+    
+    // apply args
+    str = substArguments(translations[id], args)
+    
+    // apply macros
+    return substMacros(translations, id, str, args)
+    
+    // replace {{arguments}} with their values or the
+    // associated translation string (based on its key)
+    function substArguments(str, args) {
+      var reArgs = /\{\{\s*([a-zA-Z\.]+)\s*\}\}/
+        , match
+      
+      while (match = reArgs.exec(str)) {
+        if (!match || match.length < 2)
+          return str // argument key not found
+
+        var arg = match[1]
+          , sub = ''
+        if (arg in args) {
+          sub = args[arg]
+        } else if (arg in translations) {
+          sub = translations[arg]
+        } else {
+          consoleWarn('Could not find argument {{' + arg + '}}')
+          return str
+        }
+
+        str = str.substring(0, match.index) + sub + str.substr(match.index + match[0].length)
+      }
+      
+      return str
+    }
+    
+    // replace {[macros]} with their values
+    function substMacros(key, str, args) {
+      var regex = /\{\[\s*([a-zA-Z]+):([a-zA-Z]+)\s*\]\}/;
+        , match = reIndex.exec(str);
+      if (!match || !match.length)
+        return str;
+
+      // a macro has been found
+      // Note: at the moment, only one parameter is supported
+      var macroName = reMatch[1]
+        , paramName = reMatch[2]
+      
+      if (!(macroName in gMacros)) return str
+      
+      var param
+      if (args && paramName in args) {
+        param = args[paramName]
+      } else if (paramName in translations) {
+        param = translations[paramName]
+      }
+
+      // there's no macro parser yet: it has to be defined in gMacros
+      var macro = trl8.macros[macroName]
+      str = macro(translations, key, str, param)
+      return str
+    }
+  }
+  
   /**
    * Applies translations to a DOM node (recursive)
    */
@@ -229,17 +734,16 @@ window.trl8 = (function(window, document, undefined) {
       }
     }
     
-    // apply args
-    str.str = substArguments(translations[str.id], str.args)
-
+    str.str = trl8.get(str.id, str.args)
+    
     var prop
       , index = str.id.lastIndexOf('.')
       , attrList = // allowed attributes
-      { "title": 1
-      , "innerHTML": 1
-      , "alt": 1
-      , "textContent": 1
-      }
+       { "title": 1
+       , "innerHTML": 1
+       , "alt": 1
+       , "textContent": 1
+       }
     // get attribute name to apply str to
     if (index > 0 && str.id.substr(index + 1) in attrList) { // an attribute has been specified
       prop = str.id.substr(index + 1)
@@ -266,34 +770,6 @@ window.trl8 = (function(window, document, undefined) {
       if (!found) {
         consoleWarn('Unexpected error: could not translate element content')
       }
-    }
-
-
-    // replace {{arguments}} with their values or the
-    // associated translation string (based on its key)
-    function substArguments(str, args) {
-      var reArgs = /\{\{\s*([a-zA-Z\.]+)\s*\}\}/
-        , match
-      
-      while (match = reArgs.exec(str)) {
-        if (!match || match.length < 2)
-          return str // argument key not found
-
-        var arg = match[1]
-          , sub = ''
-        if (arg in args) {
-          sub = args[arg]
-        } else if (arg in translations) {
-          sub = translations[arg]
-        } else {
-          consoleWarn('Could not find argument {{' + arg + '}}')
-          return str
-        }
-
-        str = str.substring(0, match.index) + sub + str.substr(match.index + match[0].length)
-      }
-      
-      return str
     }
   }
   
@@ -340,5 +816,9 @@ window.trl8 = (function(window, document, undefined) {
    document.addEventListener('DOMContentLoaded', trl8.index, false)
   else if (window.attachEvent)
     document.attachEvent('onload', trl8.index, false)
+
+  // gettext-like shortcut
+  if (window._ === undefined)
+    var _ = trl8.get;
 
 })(window, document)
